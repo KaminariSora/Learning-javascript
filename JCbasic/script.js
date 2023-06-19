4cef542188011d4ba0f24dd4ee1a45fd85c15b6c
Thunder <nonthacha.h@gmail.com>
2023-06-19 22:23:32 +0700

    update

---
+++ JCbasic/script.js
@@ -0,0 +1,13 @@
+let name = "KaminariSora";
+let age = 19;
+let NAME = "javascript";
+
+document.write("<b><h1>Hello World</h1></b>");
+document.write("<hr>");
+document.write("<p>KaminariSora</p>");
+
+alert("Hi friends");
+console.log(name);
+console.log("age = ",age);
+console.error("Error");
+console.warn("Warning");
