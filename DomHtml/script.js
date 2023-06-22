8a4476dee425f120e2a1af5a53af8e9386fdd4bc
Thunder <nonthacha.h@gmail.com>
2023-06-22 16:42:49 +0700

    update

--- DomHtml/script.js
+++ DomHtml/script.js
@@ -1,4 +1,30 @@
-let a = document.getElementById('demo');
-let b = document.getElementsByTagname('p');
-console.log(a);
-console.log(b);
+let get = document.getElementById('demo');
+let selector = document.querySelector('#demo');
+let mode = document.querySelector('.light');
+let menu = document.getElementById('menu');
+let count = 1;
+
+console.log(get);
+console.log(selector);
+function LightMode(){
+    mode.setAttribute('class','light');
+}
+
+function DarkMode(){
+    mode.setAttribute('class','dark');
+}
+
+function addElement(){
+    const item = document.createElement('li');
+    item.innerText = "Item " + (count++);
+    menu.appendChild(item);
+}
+
+function deleteElement(){
+    const item = document.getElementById('item');
+    menu.removeChild(item);
+}
+
+function toggleStyle(){
+    mode.classList.toggle('dark');
+}
